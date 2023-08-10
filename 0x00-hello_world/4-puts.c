e6b33d6eb9012183ca99da4d42f99766c107faca
Hegazy <h3g4zy@gmail.com>
2023-08-10 22:20:37 +0300

    9841

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* This is my first C code! */
+/* This is my first C code
+ * ! */
 int main(void)
 {
+	/*Hello*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

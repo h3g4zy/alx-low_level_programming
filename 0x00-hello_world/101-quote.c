6e9501e7863948d4261737f9270d71dc1ed16796
Hegazy <h3g4zy@gmail.com>
2023-08-10 23:00:02 +0300

    20628

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main - Entry point
+ * main - Entry point to the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }

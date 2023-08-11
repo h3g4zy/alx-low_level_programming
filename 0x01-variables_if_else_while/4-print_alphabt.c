0807eedcdd782d75da2dd77f2c68a9159968467e
Hegazy <h3g4zy@gmail.com>
2023-08-11 19:22:26 +0300

    22875

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,7 +13,8 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter != 'q' && letter != 'e') putchar(letter);
+		if (letter != 'q' && letter != 'e')
+		{putchar(letter);}
 	}
 	putchar('\n');
 

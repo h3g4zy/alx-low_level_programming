83a419a37bc8093038d72a285af3462348dfd8ec
Hegazy <h3g4zy@gmail.com>
2023-08-11 18:57:41 +0300

    21096

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,9 +18,15 @@ int main(void)
 	{
 		printf("and is greater than 5");
 	}
-	else{}
-	if (n == 0) printf("and is 0");
-	if (n < 6 && n != 0) printf("and is less than 6 and not 0");
+	else if (n == 0)
+	{	
+		printf("and is 0");
+	}
+	else if (n < 6 && n != 0) 
+	{
+		printf("and is less than 6 and not 0");
+	}
+
 	printf("\n");
 	return (0);
 }

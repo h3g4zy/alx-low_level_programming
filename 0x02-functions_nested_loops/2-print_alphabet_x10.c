95295d7a8f8da9b18941e096a31f004d8296320f
Hegazy <h3g4zy@gmail.com>
2023-08-16 00:33:39 +0300

    22986

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,12 @@
 void print_alphabet_x10(void)
 {
 	int i = 0;
-	
-	for(i = 1; i <= 10; i++)
+
+	for (i = 1; i <= 10; i++)
 	{
 		char z;
-		for(z = 'a'; z <= 'z'; z++)
+
+		for (z = 'a'; z <= 'z'; z++)
 		{
 			_putchar(z);
 		}

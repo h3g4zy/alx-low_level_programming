7f12ea2c39a387647d608b35f00f0955c060f03b
Hegazy <h3g4zy@gmail.com>
2023-08-16 02:28:58 +0300

    25758

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,10 +3,11 @@
 #include <string.h>
 #include <stdlib.h>
 /**
-  * jack_bauer -  function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
-  * Returns: 0
- * ab:cd 
- */
+* jack_bauer -  function that prints every minute of
+*  the day of Jack Bauer, starting from 00:00 to 23:59
+* Returns: 0
+* ab:cd 
+*/
 
 void jack_bauer(void)
 {
@@ -33,5 +34,4 @@ void jack_bauer(void)
 		}
 		}
 		}
-}
-		
+}		

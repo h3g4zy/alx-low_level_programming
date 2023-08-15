bdc42aa68f92511a84c0281fee6166b46eeefa33
Hegazy <h3g4zy@gmail.com>
2023-08-16 02:29:29 +0300

    18113

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@
 * jack_bauer -  function that prints every minute of
 *  the day of Jack Bauer, starting from 00:00 to 23:59
 * Returns: 0
-* ab:cd 
+* ab:cd
 */
 
 void jack_bauer(void)
@@ -34,4 +34,4 @@ void jack_bauer(void)
 		}
 		}
 		}
-}		
+}

4ee7e6d5e65d2007d654cb5876acf53ea4b89c5b
Hegazy <h3g4zy@gmail.com>
2023-08-16 00:40:39 +0300

    26613

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-/**  
-*      
-* Description: print_alphabet_x10    
-*       
-* Return: Always 0 (Success)	
+/**
+* Description: print_alphabet_x10
+*
+* Return: Always 0 (Success)
 */
 
 void print_alphabet_x10(void)

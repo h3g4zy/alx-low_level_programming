a1e2e11d3a3b643567dbf82cac148df30473b2e7
Hegazy <h3g4zy@gmail.com>
2023-08-16 01:08:46 +0300

    13048

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,9 @@
 /**
    * print_sign - Check for sign
     * @n: Number to be checked
-     * Return: Returns 1 and prints + if n is greater than zero. Returns 0 and prints 0 if n is zero. Returns -1 and prints - if n is less than zero
+     * Return: Returns 1 and prints + if n is greater than zero.
+     * Returns 0 and prints 0 if n is zero. 
+     * Returns -1 and prints - if n is less than zero
       */
 
 int print_sign(int n)
@@ -19,7 +21,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);

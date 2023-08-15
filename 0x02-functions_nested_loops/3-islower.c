859de5684820a698110b4ec3807823b0021f1b42
Hegazy <h3g4zy@gmail.com>
2023-08-16 00:50:14 +0300

    13045

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 
 /**
   * _islower - a function that checks for lowercase character
+  * c is the character we would check for being lowercase
   * Return: 1 if c is lowercase , 0 otherwise.
   */
 
@@ -12,5 +13,8 @@ int _islower(int c)
 	{
 		return (1);
 	}
-	else return (0);
+	else
+	{
+		return (0);
+	}
 }

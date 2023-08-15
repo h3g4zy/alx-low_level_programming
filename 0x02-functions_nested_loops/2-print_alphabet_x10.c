05870e32b5cc177397f5c0349f01ea1746f55f92
Hegazy <h3g4zy@gmail.com>
2023-08-16 00:31:10 +0300

    5981

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alhpabet 10x - Make the alphabet ten times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	
+	for(i = 0; i <= 10; i++)
+	{
+		char z;
+		for(z = 'a'; z <= 'z'; z++)
+		{
+			_putchar(z);
+		}
+		_putchar('\n');
+	}
+}

05fd9801202107ec19a793430ee635c37c88210f
Hegazy <h3g4zy@gmail.com>
2023-08-16 03:15:43 +0300

    8070

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,27 +11,50 @@ void times_table(void)
 	int col;
 	int n;
 	int x;
+	int n1, n2;
 
 	    for (col = 0; col <= 9; col++)
 		        {
 			for (x = 0; x < 10; x++)
 			{
+				n = x * col;
+			if (n > 9)
+			{
+				n2 = n % 10;
+				n1 = (n - n2) / 10;
+			}
 				if (x < 9)
-				{	
-					n = x * col;
+				{
+					if (n < 9)
+					{
 					_putchar(n + '0');
 					_putchar(',');
 					_putchar(' ');
+					}
+					else
+					{
+					_putchar(n1 + '0');
+					_putchar(n2 + '0');
+					_putchar(',');
+					_putchar(' ');
+					}
 				}
-	               else
-													                 {
-																                     _putchar(n + '0');
-																		                         _putchar('$');
-																					                 }
-
-												         }
-					        _putchar('\n');
-
-						    }
+	               		else
+		       		{
+					if (n < 9)
+					{
+					_putchar(n + '0');
+					_putchar('$');
+					}
+					else
+					{
+						_putchar(n1 + '0');
+						_putchar(n2 + '0');
+						_putchar('$');
+					}
+				}
+			}
+		        _putchar('\n');
+			}
 
 }

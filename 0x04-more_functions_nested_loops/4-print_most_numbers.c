8f1bce30bd42a77f707afc8a1d770c83eaf1ec6f
HEGAZY <h3g4zy@gmail.com>
2023-08-17 22:00:28 +0300

    16059

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
    * print_moast_numbers - prints the numbers,
    * from 0 to 9, followed b a new line.
    * except 2 and 4
-     * Return: 0
+     * Return: void
       */
 
 void print_most_numbers(void)

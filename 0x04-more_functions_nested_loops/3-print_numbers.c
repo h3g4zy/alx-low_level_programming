512195e326e68f7b42fb770493ebbba8e669ecd1
HEGAZY <h3g4zy@gmail.com>
2023-08-17 21:54:02 +0300

    21076

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-   * print_numbers - prints the numbers, 
+   * print_numbers - prints the numbers,
    * from 0 to 9, followed by a new line.
      * Return: 0
       */
@@ -12,7 +12,8 @@ void print_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0');
+	_putchar(i + '0');
 	}
+
 	_putchar('\n');
 }

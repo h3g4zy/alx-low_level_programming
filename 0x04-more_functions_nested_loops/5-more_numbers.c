1e60829f88fd2dd35058e5a3de3df2036c50d31c
HEGAZY <h3g4zy@gmail.com>
2023-08-17 22:08:48 +0300

    31650

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,14 +2,15 @@
 
 /**
  * more_numbers -  a function that prints 10
- *  times the numbers, from 0 to 14, 
+ *  times the numbers, from 0 to 14,
  *  followed by a new line.
  *  Return: 0
  */
 
 void more_numbers(void)
 {
-	int i, n;
+	int i;
+	int n;
 
 	for (i = 0; i < 10; i++)
 	{

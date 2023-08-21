47c3069e670d9563111a58b754f5de281771cf12
hegazy <h3g4zy@gmail.com>
2023-08-22 00:55:10 +0300

    30705

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - a function that reverses a string
+ * @s: input string
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+strrev(s);
+}

624e19231c1995d73856085de20aee471706cfe5
Hegazy <h3g4zy@gmail.com>
2023-08-22 00:20:03 +0300

    1385

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int para
+ * and updates the value it points to 98
+ * @n: int parameter
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

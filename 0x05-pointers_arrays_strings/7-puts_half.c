c7ad7cb20b022cf23a592d469be2f6e094e36ab0
hegazy <h3g4zy@gmail.com>
2023-08-22 01:20:48 +0300

    18842

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,19 @@ int x, y;
 
 for (x = 0; str[x] != '\0'; ++x)
 ;
+if (x % 2 == 0)
+{
 for (y = x / 2; str[y] != '\0'; y++)
 {
 _putchar(str[y]);
+}
+}
+else
+{
+  for (y = (x - 1) / 2; str[y] != '\0'; y++)
+{
+_putchar(str[y]);
+}  
 }
 _putchar('\n');
 }

e8dbc47e0777ac18b5fb1a149d899113cd7f012e
hegazy <h3g4zy@gmail.com>
2023-08-22 01:06:21 +0300

    3554

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: input string
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+int x;
+
+for (x = 0; str[x] != '\0';)
+{
+    _putchar(str[x] + '0');
+    x += 2;
+}
+}

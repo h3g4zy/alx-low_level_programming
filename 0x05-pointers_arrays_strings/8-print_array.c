f68ac229a481fa3c293e3e02927d1843900ce7dd
hegazy <h3g4zy@gmail.com>
2023-08-22 01:31:35 +0300

    9404

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_array - prints n elements of an array 
+ * print_array - prints n elements of an array
  * of integers, followed by a new line
  * @a: string par input
  * @n: elements par input

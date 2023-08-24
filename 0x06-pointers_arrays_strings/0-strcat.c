bdac6184a90df20836cbe2883ba7e4c5986aab22
hegazy <h3g4zy@gmail.com>
2023-08-24 07:18:27 +0300

    16245

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,11 +3,13 @@
 
 /**
  * _strcat - a function that concatenates two strings.
- * Return: Always 0.
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to destination string
  */
 
 char *_strcat(char *dest, char *src)
 {
 char *ptr = strcat(dest, src);
-return ptr;
+return (ptr);
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - a function that concatenates two strings.
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be concated
+ * Return: pointer to destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int c, i;
+
+/* Count characters of destnation string */
+while (dest[c])
+c++;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+    dest[c + i] = src[i];
+}
+dest[c + i] = '\0';
+
+return (dest);
+}

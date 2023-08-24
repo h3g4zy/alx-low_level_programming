a12100b7136228302a947be3f9635e907e96acf6
hegazy <h3g4zy@gmail.com>
2023-08-24 06:58:08 +0300

    12282

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - a function that concatenates two strings.
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char *ptr = strcat(*dest, *src);
+return *ptr;
+}

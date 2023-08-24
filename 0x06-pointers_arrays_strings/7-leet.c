bdd97dbcd4b8952468cf32589f11df4f631b81e1
hegazy <h3g4zy@gmail.com>
2023-08-24 09:07:45 +0300

    21923

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <ctype.h>
 
-
 /**
  * leet - encodes a string into 1337
  * @s: string to be manipulated
@@ -13,7 +12,7 @@ char *leet(char *s)
 {
 char *cp = s;
 char key[] = {'A', 'E', 'O', 'T', 'L'};
-int value[] = {4, 3, 0, 7 ,1};
+int value[] = {4, 3, 0, 7 , 1};
 unsigned int i;
 
 while (*s)

b386b834d0d4dd020820598544ff8defd118095f
hegazy <h3g4zy@gmail.com>
2023-08-28 22:27:58 +0300

    16913

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - searches a string for 
+ * any of a set of bytes
+ * @s: string
+ * @accept: string containing the
+ * characters to match
+ * Return: initial matching length
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+char *ret;
+
+ret = strpbrk(s, accept);
+
+return (ret);
+}

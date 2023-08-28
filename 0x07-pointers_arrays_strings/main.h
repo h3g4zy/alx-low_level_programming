1354d7a35e07bd7df9d7aa943b4b88ae75744289
hegazy <h3g4zy@gmail.com>
2023-08-28 22:19:18 +0300

    9755

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,4 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-<<<<<<< HEAD
 unsigned int _strspn(char *s, char *accept);
-=======
->>>>>>> dcf79c29420b1494b584680d23ad60e4791e671d

7abb0778c4660e470776ff88b6801939ab05126d
HEGAZY <h3g4zy@gmail.com>
2023-08-30 20:05:44 +0300

    15979

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * puts_recursion - prints a string 
+ * puts_recursion - prints a string
  * followed by a new line
  * @s: string to be printed
- * 
- * Return: Nothing
+ * Return: void
  */
 
 void _puts_recursion(char *s)

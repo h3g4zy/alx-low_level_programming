23bfb33f6c5c492fb24e726da3cde2eb9cad2812
hegazy <h3g4zy@gmail.com>
2023-09-13 01:42:25 +0300

    14915

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG
+#define _DOG
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner
+*/
+
+struct dog
+{
+char * name;
+float age;
+char * owner;
+};
+#endif

6f2ea57116b02b3ef96fb3349cca92df5721b6bb
Hegazy <h3g4zy@gmail.com>
2023-09-22 21:08:41 +0300

    17307

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,9 +13,9 @@ void free_list(list_t *head)
 
     while (current != NULL)
     {
-        next = current->next;  // Store the next node pointer
-        free(current->str);   // Free the string (if not NULL)
-        free(current);        // Free the node itself
-        current = next;       // Move to the next node
+        next = current->next;
+        free(current->str);
+        free(current);
+        current = next;
     }
 }

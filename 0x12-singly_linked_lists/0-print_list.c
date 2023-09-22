f40c62de0ed013d661ed49eb0018b75ceb506298
Hegazy <h3g4zy@gmail.com>
2023-09-22 20:47:12 +0300

    3935

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,18 +8,18 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+    unsigned long int x = 0;
 
     while (h != NULL)
     {
         if (h->str == NULL)
-            printf("(nil)");
+            printf("[0] (nil)\n");
         else
-            printf("[%lu] %s\n", strlen(h->str), h->str);
+            printf("[%d] %s\n", h->len, h->str);
 
         h = h->next;
-        node_count++;
+        x++;
     }
 
-    return (node_count);
+    return (x);
 }

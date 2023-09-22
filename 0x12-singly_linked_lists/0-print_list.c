38046c8e528bde389f6b8db4ac320d1c8bafcffb
Hegazy <h3g4zy@gmail.com>
2023-09-22 04:19:55 +0300

    21966

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: A pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_list(const list_t *h)
+{
+    size_t node_count = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("(nil)");
+        else
+            printf("[%d] %s\n", strlen(h->str), h->str);
+
+        h = h->next;
+        node_count++;
+    }
+
+    return (node_count);
+}

c31add92c12e41381354e3bff2fee317e932038d
Hegazy <h3g4zy@gmail.com>
2023-09-22 04:29:01 +0300

    20765

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,11 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t node_count = 0;
-
-    while (h != NULL)
-    {
-        node_count++;
-        h = h->next;
-    }
-
-    return (node_count);
+size_t node_count = 0;
+while (h != NULL)
+{
+node_count++;
+h = h->next;
+}
+return (node_count);
 }

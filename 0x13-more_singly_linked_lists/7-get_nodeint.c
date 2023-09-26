9dabb6290a101ca5bbd13a8fc172575b33b9d5ed
Hegazy <h3g4zy@gmail.com>
2023-09-26 21:02:29 +0300

    2776

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,18 +12,18 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int count = 0;
-    listint_t *current = head;
+unsigned int count = 0;
+listint_t *current = head;
 
-    while (current != NULL)
-    {
-        if (count == index)
-        {
-            return (current);
-        }
-        current = current->next;
-        count++;
-    }
+while (current != NULL)
+{
+if (count == index)
+{
+return (current);
+}
+current = current->next;
+count++;
+}
 
-    return (NULL);
+return (NULL);
 }

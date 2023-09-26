3b516b8c79e82df18c5a2b50989539b570c4357e
Hegazy <h3g4zy@gmail.com>
2023-09-26 20:59:00 +0300

    5284

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * get_nodeint_at_index - deletes the head node of
+  * a listint_t linked list, and returns
+  * the head node’s data (n)
+  * @head: pointer to pointer of listint_t
+  * @index: index of node
+  * 
+  * Return: pointer to node
+  */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+    unsigned int n;
+
+    for (node = head, n = 0; node && n < index; node->next, n++)
+            ;
+    return(node);
+   
+}

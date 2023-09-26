da342af66a9f2b91b21bb99de0c91766055b8988
Hegazy <h3g4zy@gmail.com>
2023-09-26 20:46:18 +0300

    2292

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * add_nodeint - adds a new node at the 
+  * add_nodeint_end - adds a new node at the 
   * end of a listint_t list
   * @head: pointer to pointer of listint_t
   * @n: value of new node

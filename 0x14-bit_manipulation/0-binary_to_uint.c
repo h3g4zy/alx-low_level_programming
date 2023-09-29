e16fb9cc010a570cb24ccf657ac1db239eafeb6a
HEGAZY <h3g4zy@gmail.com>
2023-09-29 03:48:49 +0300

    7660

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 
 	if (b == NULL)
-		return 0;
+	return (0);
 
 	while (b[i] == '0' || b[i] == '1')
 	{
@@ -22,5 +22,5 @@ unsigned int binary_to_uint(const char *b)
 		val += b[i] - '0';
 		i++;
 	}
-	return val;
+	return (val);
 }

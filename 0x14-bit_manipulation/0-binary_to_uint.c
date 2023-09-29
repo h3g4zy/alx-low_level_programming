7d10bfaa0d97dff15cb141059aa73544631eb522
HEGAZY <h3g4zy@gmail.com>
2023-09-29 03:45:43 +0300

    32247

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary number to 
+ * an unsinged int
+ * @b: pointer to string of binary numbers
+ *
+ * Return: Unsigned integer
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int val = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return 0;
+
+	while (b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
+	}
+	return val;
+}

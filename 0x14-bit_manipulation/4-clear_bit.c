49a8b193c57e50d6a7bf542b3a573075bf602194
HEGAZY <h3g4zy@gmail.com>
2023-09-29 04:51:44 +0300

    23367

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,9 +14,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 unsigned long int bitmask = 1ul << index;
 if (index >= sizeof(unsigned long int) * 8)
 return (-1);
-
 *n &= ~bitmask;
-
 return (1);
-
 }

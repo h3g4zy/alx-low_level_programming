c3de46bf3d71bd3978702275122a00487b75bf11
Hegazy <h3g4zy@gmail.com>
2023-10-03 00:37:44 +0300

    25257

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,19 +12,22 @@ int fd_from, fd_to;
 ssize_t bytes_read, bytes_written;
 char buffer[READ_BUF_SIZE];
 
-if (argc != 3) {
+if (argc != 3)
+{
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 return (97);
 }
 
 fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1) {
+if (fd_from == -1)
+{
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 return (98);
 }
 
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (fd_to == -1) {
+if (fd_to == -1)
+{
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 close(fd_from);
 return (99);

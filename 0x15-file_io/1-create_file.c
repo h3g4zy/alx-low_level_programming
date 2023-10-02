02ff3f64da74b64b8fdc5a986a2e3af09be3ca75
Hegazy <h3g4zy@gmail.com>
2023-10-03 00:28:31 +0300

    6403

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,23 +10,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (filename == NULL)
-        return (-1);
+int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-    if (fd == -1)
-        return (-1);
+if (filename == NULL)
+return (-1);
 
-    if (text_content != NULL) {
-        size_t len = strlen(text_content);
-        ssize_t bytes_written = write(fd, text_content, len);
+if (fd == -1)
+return (-1);
 
-        if (bytes_written == -1) {
-            close(fd);
-            return (-1);
-        }
-    }
+if (text_content != NULL)
+{
+size_t len = strlen(text_content);
+ssize_t bytes_written = write(fd, text_content, len);
 
-    close(fd);
-    return (1);
+if (bytes_written == -1)
+{
+close(fd);
+return (-1);
+}
+}
+
+close(fd);
+return (1);
 }
